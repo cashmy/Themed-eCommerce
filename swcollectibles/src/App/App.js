562abc8e44a4482964da2dd6225d313ead0c307b
cashmy <cmyers880@gmail.com>
2021-05-16 16:08:11 -0500

    Added Footer component

--- swcollectibles/src/App/App.js
+++ swcollectibles/src/App/App.js
@@ -3,6 +3,7 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 //import MiniDrawer from './MiniDrawer/miniDrawer'
 import AppBar from '../components/AppBar/AppBar.js'
 import SignInSide from '../components/SignInSide/SignInSide.js'
+import Footer from '../components/Footer/Footer.js'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 
@@ -16,6 +17,7 @@ const App = () => {
         <AppBar />
         <SignInSide />
         {/* <MiniDrawer /> */}
+        <Footer />
       </Router>
     </div>
   );
