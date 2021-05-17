80f5f3676019c8863c4a63810240cda99efd24db
cashmy <cmyers880@gmail.com>
2021-05-17 14:57:24 -0500

    Add Products Model & Controller

---
+++ eCommerceStarterCode/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using eCommerceStarterCode.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace eCommerceStarterCode.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // <baseurl>/api/products
+        [HttpGet, Authorize]
+        public IActionResult Get()
+        {
+            var products = _context.Products;
+            return Ok(products);
+        }
+
+        // POST /api/product
+        [HttpPost, Authorize]
+        public IActionResult Post([FromBody]Products value)
+        {
+            _context.Products.Add(value);
+            _context.SaveChanges();
+            return StatusCode(201, value);
+        }
+    }
+}
---
+++ eCommerceStarterCode/Models/Products.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceStarterCode.Models
+{
+    public class Products
+    {
+        public int ProductId { get; set; }
+        public string ProductDescription { get; set; }
+        public decimal ProductPrice { get; set; }
+        public int QuantityOnHand { get; set; }
+        public decimal ProductAverageRating { get; set; }
+        public byte[] ProductImage { get; set; }
+        
+        //Add a foreign key to the category file
+    }
+}
