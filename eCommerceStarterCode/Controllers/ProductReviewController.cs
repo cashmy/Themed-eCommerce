23be830bc40c001dd08f9fb96267ce1e0ce2a52f
DemetriceW20 <demetrice_williams@hotmail.com>
2021-05-18 07:29:50 -0800

    Added new endpoints for ProductReviewController.

--- eCommerceStarterCode/Controllers/ProductReviewController.cs
+++ eCommerceStarterCode/Controllers/ProductReviewController.cs
@@ -27,14 +27,57 @@ namespace eCommerceStarterCode.Controllers
             return Ok(productReviews);
         }
 
+        [HttpGet("{id}-reviews"), Authorize]
+        public IActionResult UserReviews(string id)
+        {
+            try
+            {
+                var reviews = _context.ProductReviews.Where(u => u.UserId == id).Select(u => new { u.ReviewText, u.ReviewRating }).ToList();
+                return Ok(reviews);
+
+            }
+            catch {
+                return NotFound("User Not Found");
+            }
+
+        }
+
+        [HttpPut("{productId}_{id}-review"), Authorize]
+        public IActionResult EditReview(string id, int productId, [FromBody] ProductReview value)
+        {
+            try
+            {
+                var review = _context.ProductReviews.Where(u => (u.ProductId == productId && u.UserId == id)).SingleOrDefault();
+                _context.ProductReviews.Remove(review);
+                _context.ProductReviews.Add(value);
+                _context.SaveChanges();
+                return StatusCode(201, value);
+
+            }
+            catch
+            {
+                return NotFound();
+            }
+
+        }
+
         // POST /api/product
         [HttpPost("create"), Authorize]
-        public IActionResult CreateProductReview([FromBody]ProductReview value)
+        public IActionResult CreateProductReview([FromBody] ProductReview value)
         {
             /// Currently hard coded... Need to pass parameters
             _context.ProductReviews.Add(value);
             _context.SaveChanges();
             return StatusCode(201, value);
+        }
+
+        [HttpDelete("{productId}_{id}-deleteReview"), Authorize]
+        public IActionResult DeleteProductReivew(string id, int productId)
+        {
+            var review = _context.ProductReviews.Where(u => (u.UserId == id && u.ProductId == productId)).SingleOrDefault();
+            _context.Remove(review);
+            _context.SaveChanges();
+            return Ok();
         }
     }
 }
