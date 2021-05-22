60e1f4885902b990a9531f0ba63f6c72e1d1c0a4
DemetriceW20 <demetrice_williams@hotmail.com>
2021-05-22 11:55:14 -0800

    Added new Claim for isSupplier in token.

---
+++ eCommerceStarterCode/Migrations/20210522020642_migration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eCommerceStarterCode.Migrations
+{
+    public partial class migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
